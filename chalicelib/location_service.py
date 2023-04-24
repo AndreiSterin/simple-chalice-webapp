b8f679718ab5b9f98401eb68d5636bcfac5de781
ape09 <kell6054@ox.ac.uk>
2023-04-24 14:44:49 +0100

    Exeter supported

--- chalicelib/location_service.py
+++ chalicelib/location_service.py
@@ -5,4 +5,6 @@ def information_about(place):
         return "Oxford is home to the University of Oxford, the oldest university in the English-speaking world"
     if place == "Siegburg":
         return "Siegburg is a small city in the suburbs of Cologne"
+    if place == "Exeter":
+        return "Exeter is an old Roman fort"
     return f"Unknown place: {place}"
